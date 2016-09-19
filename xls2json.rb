4a4439505ef5f81ddbe71ca185c4a7f19ec1157a
haribabu <itzhari.g@gmail.com>
2016-09-19 14:57:18 -0500

    tests

--- xls2json.rb
+++ xls2json.rb
@@ -1,19 +1,18 @@
-#csvtojson3
+#csvtojson99
 #hari@serverlessly.io
-#eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NzQyNjcyODYsImlhdCI6MTQ3
+#eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NzQzMTg0MTYsImlhdCI6MTQ3NDMxNDgxNiwiaXNzIjoic2VydmVybGVzc2x5Iiwic2NvcGVzIjpbInNlcnZlcmxlc3NseXVzZXIiXSwidXNlciI6eyJlbWFpbCI6ImZqdm5ydUBnbWFpbC5jb20ifX0.jc0sprtIo1BTX4xwYy9YluXqfdIjG--O8ZwPp8uUk40
 
-require 'json' 
-require 'roo' 
-require 'roo-xls' 
+require 'json'
+require 'roo'
+require 'roo-xls'
 
-def convertcsvtojson 
-xls = nil 
-if params[:file] 
-file = params[:file][:tempfile] 
-xls = Roo::Spreadsheet.open(file) 
-end 
-return xls.to_json 
-end 
+def convertcsvtojson
+xls = nil
+if params[:file]
+file = params[:file][:tempfile]
+xls = Roo::Spreadsheet.open(file)
+end
+return xls.to_json
+end
 
-convertcsvtojson 
-
+convertcsvtojson
